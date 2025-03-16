9bcd3ee97e3fd8fb4eabf2def08029bc16f6eaad
andre-hegre <andrhegr@stud.ntnu.no>
2025-03-16 15:32:28 +0100

    "tester med miljødata hentet fra YR i data folder"

---
+++ proj_environment-main/data/API_python_command.py
@@ -0,0 +1,16 @@
+import sys
+import json
+print("Python executable:", sys.executable)
+
+import requests
+
+url="https://api.met.no/weatherapi/airqualityforecast/0.1/?lat=60&lon=10&areaclass=grunnkrets"
+headers={"User-Agent": "værdatainnsamling/1.0 (andrhegr@stud.ntnu.no)"}
+
+response=requests.get(url,headers=headers)
+if response.status_code == 200:
+    data = response.json()
+    with open("Miljoeanalyse/proj_environment-main/weather_data.json", "w") as json_file:
+        json.dump(data, json_file, indent=4)
+else: 
+    print("error")
