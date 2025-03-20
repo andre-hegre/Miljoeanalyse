3997ec82fcd4cee13ddac72fa8cbc7d1688ef05b
Martin <martiobr@stud.ntnu.no>
2025-03-20 15:56:45 +0100

    API changes

--- proj_environment-main/data/API_python_command.py
+++ proj_environment-main/data/API_python_command.py
@@ -10,17 +10,7 @@ headers={"User-Agent": "værdatainnsamling/1.0 (andrhegr@stud.ntnu.no)"}
 response=requests.get(url,headers=headers)
 if response.status_code == 200:
     data = response.json()
-<<<<<<< HEAD
-    with open("proj_environment-main\data\weather_data.json", "w") as json_file:
-        json.dump(data, json_file, indent=4)
-else: 
-    print("error") 
-    
-     
-   
-=======
     with open("proj_environment-main/weather_data.json", "w") as json_file:
         json.dump(data, json_file, indent=4)
 else: 
-    print("error")
->>>>>>> 039a2650971fbe9fd2028d9c40763aec7e231113
+    print("error") 
