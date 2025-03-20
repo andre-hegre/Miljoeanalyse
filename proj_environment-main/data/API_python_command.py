ec06f7a7d687f2f5f864b18b73a3398533e44ffb
Martin <martiobr@stud.ntnu.no>
2025-03-20 15:11:30 +0100

    Reader

--- proj_environment-main/data/API_python_command.py
+++ proj_environment-main/data/API_python_command.py
@@ -10,7 +10,7 @@ headers={"User-Agent": "værdatainnsamling/1.0 (andrhegr@stud.ntnu.no)"}
 response=requests.get(url,headers=headers)
 if response.status_code == 200:
     data = response.json()
-    with open("Miljoeanalyse/proj_environment-main/weather_data.json", "w") as json_file:
+    with open("proj_environment-main/weather_data.json", "w") as json_file:
         json.dump(data, json_file, indent=4)
 else: 
     print("error")
