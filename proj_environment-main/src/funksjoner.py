cf3f13983593ebbf4bad9aeda14cf33e2dbb671f
andre-hegre <andrhegr@stud.ntnu.no>
2025-05-20 15:00:57 +0200

    +func

--- proj_environment-main/src/funksjoner.py
+++ proj_environment-main/src/funksjoner.py
@@ -15,4 +15,23 @@ def introduser_uteliggere(df , kolonne="mean(air_temperature P1D)" , frac=0.04):
     df.loc[df.sample(frac=frac).index, kolonne] = 570
     return df
 
+def håndter_mangler(df,column,method,degree=1):
+    df=df.copy()
+    if method=='median':
+        df[column]=df[column].fillna(df[column].median())
+    elif method == 'interpolate':
+        df[column] = df[column].interpolate(method='polynomial',order=degree)
+    elif method == 'ffill':
+        df[column] = df[column].fillna(method='ffill')
+    elif method == 'bfill':
+        df[column] = df[column].fillna(method='bfill')
+    else: 
+        df[column] = df[column].fillna(df[column].mean())
+    return df
 
+
+def håndter_uteliggere(df,column,upper=100,lower=-50):
+    df=df.copy()
+    
+    df[column]=np.where((df[column]<lower) | (df[column]>upper),np.nan,df[column])
+    return df
