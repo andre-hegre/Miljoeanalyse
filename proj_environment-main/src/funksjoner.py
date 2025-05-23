1aa209184cd91f722774066cb6223987b4100e47
andre-hegre <andrhegr@stud.ntnu.no>
2025-05-23 11:46:24 +0200

    upd unit-test

--- proj_environment-main/src/funksjoner.py
+++ proj_environment-main/src/funksjoner.py
@@ -4,21 +4,32 @@ import numpy as np
 
 # introduserer uregelmessigheter (NaN til omtrent 6% av verdiene i valgt kolonne)
 def introduser_NaN(df , kolonne="mean(relative_humidity P1D)" , frac=0.06):
-
     df=df.copy()
+    if frac>1 or frac<0:
+        raise ValueError("Frac must be a value between 0 and 1")
+    if not isinstance(df, pd.DataFrame):
+        raise TypeError("df input must be pandas dataframe")
+    if kolonne not in df.columns:
+        raise ValueError(f"Column {kolonne} not found in df Dataframe")
     df.loc[df.sample(frac=frac).index, kolonne] = np.nan 
     return df
 
 # introduserer enkelte tallverdier på 570, som ikke gir mening (uteliggere)
 def introduser_uteliggere(df , kolonne="mean(air_temperature P1D)" , frac=0.04):
     df=df.copy()
+    if frac>1 or frac<0:
+        raise ValueError("Frac must be a value between 0 and 1")
+    if not isinstance(df, pd.DataFrame):
+        raise TypeError("df input must be pandas dataframe")
+    if kolonne not in df.columns:
+        raise ValueError(f"Column {kolonne} not found in df Dataframe")
     df.loc[df.sample(frac=frac).index, kolonne] = 570
     return df
 
 def håndter_mangler(df,column,method,degree=1):
     df=df.copy()
     if column not in df:
-        raise ValueError(f"Column {column} not in {df}")
+        raise ValueError(f"Column {column} not found in df Dataframe")
     try: 
 
         if method=='median':
@@ -38,6 +49,12 @@ def håndter_mangler(df,column,method,degree=1):
 
 def håndter_uteliggere(df,column,upper=100,lower=-50):
     df=df.copy()
+    if upper<=lower:
+        raise TypeError("Upper boundary not be equal to or lower than lower boundary")
+    if not isinstance(df, pd.DataFrame):
+        raise TypeError("df input must be pandas dataframe")
+    if column not in df:
+        raise ValueError(f"Column {column} not found in df Dataframe")
     
     df[column]=np.where((df[column]<lower) | (df[column]>upper),np.nan,df[column])
     return df
--- proj_environment-main/tests/Unit_testing.py
+++ proj_environment-main/tests/Unit_testing.py
@@ -1,10 +1,31 @@
 import sys
 import pandas as pd
 import numpy as np
+import os
+import unittest
 
-sys.path.append('../src')
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
 from funksjoner import introduser_NaN
 
-def test_introduser_NaN():
-    return 
+class test_introduserNaN(unittest.TestCase):
+    def test_positiv(self):
+        df=pd.DataFrame({'Data': [1,2,3,4,5]})
+        df_NaN=introduser_NaN(df, kolonne='Data', frac=0.4)
+        self.assertEqual(df_NaN['A'].isna().sum,2)
+    def test_invalid_frac(self):
+        df = pd.DataFrame({"A": [1, 2, 3]})
+        with self.assertRaises(ValueError):
+            introduser_NaN(df, kolonne="A", frac=1.5)
+
+    def test_invalid_df(self):
+        with self.assertRaises(TypeError):
+            introduser_NaN([1, 2, 3], kolonne="A", frac=0.1)
+
+    def test_missing_column(self):
+        df = pd.DataFrame({"A": [1, 2, 3]})
+        with self.assertRaises(ValueError):
+            introduser_NaN(df, kolonne="B", frac=0.1)
+
+if __name__ == "__main__":
+    unittest.main()
